d97454e7eb40d07e1e2de6136aa50205fcb1bace
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-11-11 11:05:22 +0200

    Add default text on empty car list

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,12 +1,52 @@
+import { useResolvedPath } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import CarCard from "components/CarCard/CarCard";
-import { ListUl } from "./CarList.styled";
+import { CatalogLink, DefaultMainTextP, DefaultTextWrapperDiv, ListUl } from "./CarList.styled";
 
 export default function CarList({ cars }) {
+  const { pathname } = useResolvedPath();
+
+  const generateDefaultText = () => {
+    switch (pathname) {
+      case "/catalog":
+        return (
+          <>
+            <DefaultMainTextP>
+              Sorry, we couldn&apos;t find any adds matching your query...
+            </DefaultMainTextP>
+
+            <p>Try to change the filter parameters to find a match!</p>
+          </>
+        );
+
+      case "/favorite":
+        return (
+          <>
+            <DefaultMainTextP>You don&apos;t have any favorite cars yet...</DefaultMainTextP>
+
+            <p>
+              Go to <CatalogLink to="../catalog">Catalog</CatalogLink> to look for the best match!
+            </p>
+          </>
+        );
+
+      default:
+        break;
+    }
+  };
+
   return (
     <>
-      <ListUl>{cars.length > 0 && cars.map(car => <CarCard key={car.id} car={car} />)}</ListUl>
+      {cars.length === 0 && <DefaultTextWrapperDiv>{generateDefaultText()}</DefaultTextWrapperDiv>}
+
+      {cars.length > 0 && (
+        <ListUl>
+          {cars.map(car => (
+            <CarCard key={car.id} car={car} />
+          ))}
+        </ListUl>
+      )}
     </>
   );
 }
--- src/components/CarList/CarList.styled.js
+++ src/components/CarList/CarList.styled.js
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
 
 export const ListUl = styled.ul`
   display: flex;
@@ -22,4 +23,31 @@ export const ListUl = styled.ul`
 
     margin-bottom: 100px;
   }
+`;
+
+export const DefaultTextWrapperDiv = styled.div`
+  font-size: 20px;
+  line-height: ${24 / 20};
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    width: 584px;
+
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.desktop}) {
+    width: 1180px;
+  }
+`;
+
+export const DefaultMainTextP = styled.p`
+  font-weight: 500;
+  color: ${({ theme }) => theme.color.blue};
+  margin-bottom: 16px;
+`;
+
+export const CatalogLink = styled(Link)`
+  color: ${({ theme }) => theme.color.blue};
+  text-decoration: underline;
 `;
