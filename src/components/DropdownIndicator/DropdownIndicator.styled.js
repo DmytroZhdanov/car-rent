bfd4189e3cc5d5bc0468c22719bf412be95ccce2
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-12-04 17:41:56 +0200

    Migrate components to TypeScript

--- src/components/DropdownIndicator/DropdownIndicator.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Icon = styled.svg`
-  width: 20px;
-  height: 20px;
-
-  fill: transparent;
-  stroke: ${({ theme }) => theme.color.blackMain};
-`;
