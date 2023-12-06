391e0784712212cb4b13158a956fa6ec1c297836
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-12-06 15:28:52 +0200

    Fix i18next types

--- @types/i18next.d.ts
+++ @types/i18next.d.ts
@@ -1,31 +1,13 @@
 import resources from "./resources";
 
-// declare module "react-i18next" {
-//   interface CustomTypeOptions {
-//     resources: {
-//       translation: {
-//         home: typeof homeEn;
-//         catalog: typeof catalogEn;
-//         carCard: typeof carCardEn;
-//         carDetails: typeof carDetailsEn;
-//         carList: typeof carListEn;
-//         filter: typeof filterEn;
-//         navBar: typeof navBarEn;
-//         pageTitle: typeof pageTitleEn;
-//         priceControl: typeof priceControlEn;
-//       };
-//     };
-//   }
-// }
-
-// declare module "i18next" {
-//   interface CustomTypeOptions {
-//     returnNull: false;
-//   }
-// }
+declare module "react-i18next" {
+  interface CustomTypeOptions {
+    resources: typeof resources;
+  }
+}
 
 declare module "i18next" {
   interface CustomTypeOptions {
-    resources: typeof resources;
+    returnNull: false;
   }
 }
